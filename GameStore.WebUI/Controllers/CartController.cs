0a60646c3290d2301105a5a1d09724fa835f3e16
Wizardkpb <wizardkpb@gmail.com>
2016-11-11 21:36:26 +0200

    First commit

---
+++ GameStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.Domain.Entities;
+using GameStore.Domain.Abstract;
+using GameStore.WebUI.Models;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IGameRepository repository;
+        private IOrderProcessor orderProcessor;//order
+        public CartController(IGameRepository repo, IOrderProcessor processor)
+        {
+            repository = repo;
+            //order
+            orderProcessor = processor;
+        }
+
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int gameId, string returnUrl)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                cart.AddItem(game, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int gameId, string returnUrl)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                cart.RemoveLine(game);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
+        //public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+        /*
+                public Cart GetCart()
+                {
+                    Cart cart = (Cart)Session["Cart"];
+                    if (cart == null)
+                    {
+                        cart = new Cart();
+                        Session["Cart"] = cart;
+                    }
+                    return cart;
+                }*/
+
+        //order
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Извините, ваша корзина пуста!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
+        }
+    }
+}
